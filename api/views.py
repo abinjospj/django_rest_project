c145299ea063840f2a0138788369690badea5856
abinjospj <abinjospj0722@gmail.com>
2026-05-21 21:36:43 +0530

    django_rest GET, POST, PUT, DELETE  method

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from students .models import Student
 from .serializers import StudentSerializer
@@ -14,10 +14,34 @@ from rest_framework.decorators import api_view
 #     # return JsonResponse(students_list, safe=False)
 
 
-@api_view(['GET',])
+@api_view(['GET', 'POST'])
 def studentsView(request):
     if request.method == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','PUT', 'DELETE'])
+def StudentDetailView(request, id):
+    student = get_object_or_404(Student, id=id)
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
